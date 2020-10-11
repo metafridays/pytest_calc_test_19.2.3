e71ae9d8b09bacb90b8218bc03db587d28fae904
metafridays <metafridays@live.com>
2020-10-11 14:20:41 +0300

    first commit

---
+++ tests/calc_test.py
@@ -0,0 +1,18 @@
+from app.calculator import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, 6, 2) == 3
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self, 3, 4) == 7
+
+    def test_substraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, 10, 5) == 5
+
